294a4eb5397dd8c7408a88aff52b65f352da9354
LZPING <648540828@qq.com>
2020-05-21 16:25:16 +0800

    @author lzp

---
+++ js/index.js
@@ -0,0 +1,67 @@
+/**
+ * 首页轮播图功能
+ */
+(function f() {
+    let index  = 0;
+    let nodeList = document.querySelectorAll(".carousel-images ul li");
+
+    nodeList[0].classList.add("visible");
+
+    let slideList = document.querySelectorAll(".carousel-slide ul li");
+    for(let i = 0;i < slideList.length;i++){
+        slideList[i].setAttribute("index",i.toString());
+    }
+    slideList[0].classList.add("active");
+    let nextTip = function () {
+        let old = index;
+        if(nodeList.length - 1 === index){
+            index = 0;
+        }else {
+            index++;
+        }
+        nodeList[old].classList.remove("visible");
+        nodeList[index].classList.add("visible");
+
+        slideList[old].classList.remove("active");
+        slideList[index].classList.add("active");
+    };
+    let previousTip = function () {
+        let old = index;
+        if(index === 0){
+            index = nodeList.length - 1;
+        }else {
+            index--;
+        }
+        nodeList[old].classList.remove("visible");
+        nodeList[index].classList.add("visible");
+
+        slideList[old].classList.remove("active");
+        slideList[index].classList.add("active");
+
+    };
+    let goToIndex = function(event) {
+        let current  = event.currentTarget;
+        if(current === slideList[index]) return;
+
+        slideList[index].classList.remove("active");
+        nodeList[index].classList.remove("visible");
+        current.classList.add("active");
+        index = new Number(current.getAttribute("index"));
+        nodeList[index].classList.add("visible");
+
+    };
+    setInterval(()=>{
+        let next = document.querySelector("#next-btn");
+        next.onclick();
+    },3000);
+    window.onload = function () {
+        let previous = document.querySelector("#previous-btn");
+        let next = document.querySelector("#next-btn");
+        previous.onclick = previousTip;
+        next.onclick = nextTip;
+        slideList.forEach((e)=>{
+            e.onclick = goToIndex;;
+        })
+    };
+})();
+
