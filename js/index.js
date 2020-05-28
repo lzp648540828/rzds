fa990bdfd9f9dd7d64a0817143bbab5e059332a8
LZPING <648540828@qq.com>
2020-05-28 15:20:47 +0800

    @author lzp

--- js/index.js
+++ js/index.js
@@ -63,11 +63,72 @@
             let next = document.querySelector("#next-btn");
             next.onclick();
         },3000);
-    };
+    }
     window.addEventListener("load",load);
 })();
 
 (function () {
+    let index  = 0;
+    let nodeList = document.querySelectorAll(".carousel .images  li");
 
+    nodeList[0].classList.add("visible");
+
+    let slideList = document.querySelectorAll(".carousel .carousel-index li");
+    for(let i = 0;i < slideList.length;i++){
+        slideList[i].setAttribute("index",i.toString());
+    }
+    slideList[0].classList.add("active");
+    let nextTip = function () {
+        let old = index;
+        if(nodeList.length - 1 === index){
+            index = 0;
+        }else {
+            index++;
+        }
+        nodeList[old].classList.remove("visible");
+        nodeList[index].classList.add("visible");
+
+        slideList[old].classList.remove("active");
+        slideList[index].classList.add("active");
+    };
+    let previousTip = function () {
+        let old = index;
+        if(index === 0){
+            index = nodeList.length - 1;
+        }else {
+            index--;
+        }
+        nodeList[old].classList.remove("visible");
+        nodeList[index].classList.add("visible");
+
+        slideList[old].classList.remove("active");
+        slideList[index].classList.add("active");
+
+    };
+    let goToIndex = function(event) {
+        let current  = event.currentTarget;
+        if(current.getAttribute("index") === index.toString()) return;
+
+        slideList[index].classList.remove("active");
+        nodeList[index].classList.remove("visible");
+        current.classList.add("active");
+        index = new Number(current.getAttribute("index")).valueOf();
+        nodeList[index].classList.add("visible");
+    };
+
+    function load() {
+        let previous = document.querySelector("#famous-btn-previous");
+        let next = document.querySelector("#famous-btn-next");
+        previous.onclick = previousTip;
+        next.onclick = nextTip;
+        slideList.forEach((e)=>{
+            e.onclick = goToIndex;;
+        });
+        setInterval(()=>{
+            let next = document.querySelector("#famous-btn-next");
+            next.onclick();
+        },5000);
+    };
+    window.addEventListener("load",load);
 })();
 
