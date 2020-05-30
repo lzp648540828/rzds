bd40e971de6dc28b57f2973cc1039182e14a64b7
LZPING <648540828@qq.com>
2020-05-30 14:13:54 +0800

    @author lzp

--- js/index.js
+++ js/index.js
@@ -1,134 +1,94 @@
 /**
- * 首页轮播图功能
+ * 轮播图功能
  */
-(function f() {
-    let index  = 0;
-    let nodeList = document.querySelectorAll(".carousel-images ul li");
-
-    nodeList[0].classList.add("visible");
-
-    let slideList = document.querySelectorAll(".carousel-slide ul li");
-    for(let i = 0;i < slideList.length;i++){
-        slideList[i].setAttribute("index",i.toString());
-    }
-    slideList[0].classList.add("active");
-    let nextTip = function () {
-        let old = index;
-        if(nodeList.length - 1 === index){
-            index = 0;
-        }else {
-            index++;
-        }
-        nodeList[old].classList.remove("visible");
-        nodeList[index].classList.add("visible");
-
-        slideList[old].classList.remove("active");
-        slideList[index].classList.add("active");
-    };
-    let previousTip = function () {
-        let old = index;
-        if(index === 0){
-            index = nodeList.length - 1;
-        }else {
-            index--;
-        }
-        nodeList[old].classList.remove("visible");
-        nodeList[index].classList.add("visible");
-
-        slideList[old].classList.remove("active");
-        slideList[index].classList.add("active");
-
-    };
-    let goToIndex = function(event) {
-        let current  = event.currentTarget;
-        if(current.getAttribute("index") === index.toString()) return;
-
-        slideList[index].classList.remove("active");
-        nodeList[index].classList.remove("visible");
-        current.classList.add("active");
-        index = new Number(current.getAttribute("index")).valueOf();
-        nodeList[index].classList.add("visible");
-
-    };
-
-    function load() {
-        let previous = document.querySelector("#previous-btn");
-        let next = document.querySelector("#next-btn");
-        previous.onclick = previousTip;
-        next.onclick = nextTip;
-        slideList.forEach((e)=>{
-            e.onclick = goToIndex;;
-        });
-        setInterval(()=>{
-            let next = document.querySelector("#next-btn");
-            next.onclick();
-        },3000);
-    }
-    window.addEventListener("load",load);
-})();
-
 (function () {
-    let index  = 0;
-    let nodeList = document.querySelectorAll(".carousel .images  li");
+    function carousel(itemSelect, dotSelect, preBtn, nextBtn) {
+        //记录当前显示的item的索引
+        this.index = 0;
 
-    nodeList[0].classList.add("visible");
+        //获取轮播的item和指示器
+        this.nodeList = document.querySelectorAll(itemSelect);
+        this.slideList = document.querySelectorAll(dotSelect);
+        this.preBtn = document.querySelector(preBtn);
+        this.nextBtn = document.querySelector(nextBtn);
 
-    let slideList = document.querySelectorAll(".carousel .carousel-index li");
-    for(let i = 0;i < slideList.length;i++){
-        slideList[i].setAttribute("index",i.toString());
+        //把第一个item显示出来
+        this.nodeList[0].classList.add("visible");
+        this.slideList[0].classList.add("active");
+
+        //给指示器添加一个属性，区分开来
+        for (let i = 0; i < this.slideList.length; i++) {
+            this.slideList[i].setAttribute("index", i.toString());
+        }
     }
-    slideList[0].classList.add("active");
-    let nextTip = function () {
-        let old = index;
-        if(nodeList.length - 1 === index){
-            index = 0;
+
+    //切换到上一张
+    carousel.prototype.nextTip = function () {
+        let old = this.index;
+        if (this.nodeList.length - 1 === this.index) {
+            this.index = 0;
         }else {
-            index++;
+            this.index++;
         }
-        nodeList[old].classList.remove("visible");
-        nodeList[index].classList.add("visible");
+        this.nodeList[old].classList.remove("visible");
+        this.nodeList[this.index].classList.add("visible");
 
-        slideList[old].classList.remove("active");
-        slideList[index].classList.add("active");
+        this.slideList[old].classList.remove("active");
+        this.slideList[this.index].classList.add("active");
     };
-    let previousTip = function () {
-        let old = index;
-        if(index === 0){
-            index = nodeList.length - 1;
+
+    //切换到下一张
+    carousel.prototype.previousTip = function () {
+        let old = this.index;
+        if (this.index === 0) {
+            this.index = this.nodeList.length - 1;
         }else {
-            index--;
+            this.index--;
         }
-        nodeList[old].classList.remove("visible");
-        nodeList[index].classList.add("visible");
+        this.nodeList[old].classList.remove("visible");
+        this.nodeList[this.index].classList.add("visible");
 
-        slideList[old].classList.remove("active");
-        slideList[index].classList.add("active");
-
+        this.slideList[old].classList.remove("active");
+        this.slideList[this.index].classList.add("active");
     };
-    let goToIndex = function(event) {
+
+
+    //点击指示器切换到对应item
+    carousel.prototype.goToIndex = function (event) {
+        //点击的获取指示器
         let current  = event.currentTarget;
-        if(current.getAttribute("index") === index.toString()) return;
 
-        slideList[index].classList.remove("active");
-        nodeList[index].classList.remove("visible");
+        //如果点击的是正展示的，什么也不做
+        if (current.getAttribute("index") === this.index.toString()) return;
+
+        //隐藏当前
+        this.slideList[this.index].classList.remove("active");
+        this.nodeList[this.index].classList.remove("visible");
+
+        //显示点击的，并更新索引
         current.classList.add("active");
-        index = new Number(current.getAttribute("index")).valueOf();
-        nodeList[index].classList.add("visible");
+        this.index = Number(current.getAttribute("index")).valueOf();
+        this.nodeList[this.index].classList.add("visible");
     };
 
-    function load() {
-        let previous = document.querySelector("#famous-btn-previous");
-        let next = document.querySelector("#famous-btn-next");
-        previous.onclick = previousTip;
-        next.onclick = nextTip;
-        slideList.forEach((e)=>{
-            e.onclick = goToIndex;;
+    //绑定事件
+    carousel.prototype.load = function () {
+        this.preBtn.onclick = carousel.prototype.previousTip.bind(this);
+        this.nextBtn.onclick = carousel.prototype.nextTip.bind(this);
+
+        this.slideList.forEach((e) => {
+            e.onclick = this.goToIndex.bind(this);
         });
         setInterval(()=>{
-            let next = document.querySelector("#famous-btn-next");
-            next.onclick();
+            this.nextBtn.onclick();
         },5000);
-    };
-    window.addEventListener("load",load);
+    }
+    window.carousel = carousel;
 })();
+
+new carousel(".mall-carousel .carousel-images  li", ".mall-carousel .carousel-slide li", "#previous-btn", "#next-btn").load();
+new carousel(".mall-famous .carousel .images li", ".mall-famous .carousel .carousel-index li", "#famous-btn-previous", "#famous-btn-next").load();
+new carousel(".mall-house-case .images li", ".mall-house-case .hc-slide li", "#hc-pre-btn", "#hc-next-btn").load();
+
+
 
